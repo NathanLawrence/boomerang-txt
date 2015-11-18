078e5c14ff43b46b123df53ab2bb29efe0ba7a9c
Nathan Lawrence <lawrencen@missouri.edu>
2015-11-18 09:19:31 -0600

    Add parsing

--- boomerang/parser.py
+++ boomerang/parser.py
@@ -1,1 +1,11 @@
 #Text Command Parser for received commands
+
+import re
+
+def parseCommand(cmdStr):
+	if re.match("(.+) every (\d+) dest (\d+)",cmdStr):
+		print "true"
+	else:
+		print "oops"
+
+parseCommand("testing every 12 dest 3035524795")
